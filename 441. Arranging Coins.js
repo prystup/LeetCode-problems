b7a3aa107029241b76d2f848ea76a27a4697a385
Prystup <prustyp910@gmail.com>
2021-11-06 12:11:15 +0200

    change name

--- 441. Arranging Coins.js
+++ 441. Arranging Coins.js
@@ -1,4 +1,4 @@
-// 441. Two Sum
+// 441. Arranging Coins
 // Problem: https://leetcode.com/problems/arranging-coins/
 
 /* You have n coins(@) and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
